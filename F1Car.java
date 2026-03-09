0eaac466c68d08f4d7b9b0ebc4a61500614cc507
ca <nayla.arsha.adyuta@gmail.com>
2026-03-09 07:51:22 +0700

    F1Car.java

--- F1Car.java
+++ F1Car.java
@@ -1,23 +1,19 @@
 class F1Car {
 
-    // Encapsulation
     private String team;
     private String model;
     private String color;
 
-    // Constructor
     public F1Car(String team, String model, String color) {
         this.team = team;
         this.model = model;
         this.color = color;
     }
 
-    // Abstraction
     public void accelerate(int speedIncrease) {
         System.out.println(team + " accelerates +" + speedIncrease + " km/h");
     }
 
-    // Getter
     public String getTeam() { return team; }
     public String getModel() { return model; }
     public String getColor() { return color; }
